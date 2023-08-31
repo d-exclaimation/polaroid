8ac69aa1d2690d158952e3c1912823c05304dbdf
Vincent <thisoneis4business@gmail.com>
2023-08-31 23:07:01 +1200

    idk

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
-import { StatusBar } from "expo-status-bar";
+import { Camera, CameraType } from "expo-camera";
 import { SafeAreaView, Text } from "react-native";
 
 export default function App() {
+  const [permission, requestPermission] = Camera.useCameraPermissions();
   return (
     <SafeAreaView className="flex-1 items-center justify-center">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Camera className="w-[300px] h-[300px]" type={CameraType.back}>
+        <Text>Camera is working!</Text>
+      </Camera>
+      <Text>{permission?.granted ? "Yes" : "Bad"}</Text>
     </SafeAreaView>
   );
 }
